eb9c24f7aefdad98182c2980a57a8826b18ab4a4
kfinto <to.czarnota@gmail.com>
2014-12-22 22:52:29 +0100

    dużo rzeczy example get

--- app/src/main/java/com/example/asmathiel/cos/MainActivity.java
+++ app/src/main/java/com/example/asmathiel/cos/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.asmathiel.cos;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,7 +69,46 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            TextView browserText = (TextView) rootView.findViewById(R.id.browserText);
+
+            changText(browserText);
+
+
+            //browserText.setText((CharSequence) new RequestTask().execute("http://example.com"));
+
             return rootView;
+        }
+
+        public void changText(TextView textv) {
+            textv.setText("Text Has Been Changed");
+            BufferedReader in = null;
+            String data = null;
+
+            try{
+                HttpClient httpclient = new DefaultHttpClient();
+
+                HttpGet request = new HttpGet();
+                URI website = new URI("http://example.com");
+                request.setURI(website);
+                HttpResponse response = httpclient.execute(request);
+                in = new BufferedReader(new InputStreamReader(
+                        response.getEntity().getContent()));
+
+                // NEW CODE
+                String line;
+                while ((line = in.readLine()) != null) {
+                    textv.append(line);
+                    textv.append("\n");
+                }
+
+                textv.append(" First line: " + line);
+                // END OF NEW CODE
+
+                textv.append(" Connected ");
+            }catch(Exception e){
+                Log.e("log_tag", "Error in http connection " + e.toString());
+            }
         }
     }
 }
